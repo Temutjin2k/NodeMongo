9c3df7daa60496b1e73dde6205444a6d786e6979
Temutjin2k <tamutdzhin2006@mail.ru>
2025-02-05 13:05:36 +0500

    CRUD operation for posts

--- index.js
+++ index.js
@@ -13,29 +13,78 @@ const blogScheme = new Schema({
 const Blog = mongoose.model("blog", blogScheme);
 
 const app = express()
+app.use(express.json())
 
-app.post("/blogs", (req, res) => {
+app.post("/blogs", async (req, res) => {
+    if(!req.body) return res.status(400).json({error: "no body provided"});
 
+    const blog = new Blog(
+        {   
+            title: req.body.title, 
+            body: req.body.body, 
+            author: req.body.author
+        })
+
+    await blog.save()
+    res.sendStatus(201)
 });
 
-app.get("/blogs", (req, res) => {
-
+app.get("/blogs", async (req, res) => {
+    const blogs = await Blog.find({})
+    res.json(blogs)
 });
 
-app.get("/blogs", (req, res) => {
-
+app.get("/blogs/:id", async (req, res) => {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    if (blog) res.json(blog)
+    else res.status(400).json({error: "Blog does not exist"})  
 });
 
-app.put("/blogs", (req, res) => {
+app.put("/blogs/:id", async (req, res) => {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    if (!blog) res.status(400).json({ error: "Blog does not exist"})
+    
+    const {title, body, author} = req.body;
+    if (blog.title == title || blog.body == body || blog.author == author){
+        return res.status(400).json({ error: "No changes were made" })
+    }
 
+    await Blog.updateOne({_id: id}, {$set: {title, body, author}})
+    
+    res.json({message: "Blog updated successfully"})
 });
 
-app.delete("/blogs", (req, res) => {
-
+app.delete("/blogs/:id", async (req, res) => {
+    const id = req.params.id
+    const blog = Blog.findById(id)
+    if (!blog) return res.status(400).json({error: "blog does not exist"})
+    
+    await Blog.deleteOne({_id: id})
+    res.json({message: "blog deleted successfully"})
 });
 
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`ListenAndServe: http://localhost:${PORT}/`)
+
+async function main() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        const PORT = process.env.PORT
+        app.listen(PORT, () => {
+            console.log(`ListenAndServe: http://localhost:${PORT}/`)
+        });
+    }
+    catch(err) {
+        return console.log(err);
+    }
+}
+
+main();
+
+// Something like Gracefull shutdown
+process.on("SIGINT", async() => {
+    await mongoose.disconnect();
+    console.log("Shutting down server");
+    process.exit();
 });
